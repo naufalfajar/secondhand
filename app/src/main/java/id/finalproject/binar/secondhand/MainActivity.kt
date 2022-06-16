756ca14d6e62c9deee0dfbba4958ddfba7448527
Karyadi Mochtar <karyadi_cm13@apps.ipb.ac.id>
2022-06-16 14:27:38 +0700

    notif

--- app/src/main/java/id/finalproject/binar/secondhand/MainActivity.kt
+++ app/src/main/java/id/finalproject/binar/secondhand/MainActivity.kt
@@ -1,11 +1,24 @@
 package id.finalproject.binar.secondhand
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnShow = findViewById<Button>(R.id.btn_show)
+
+        btnShow.setOnClickListener {
+            val view = LayoutInflater.from(this).inflate(R.layout.notif_status, null)
+            val dialog = BottomSheetDialog(this)
+            dialog.setContentView(view)
+            dialog.show()
+        }
     }
 }
