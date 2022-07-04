38a5a8656d6d66f36b5d9890f6d2b83e1806bf27
Karyadi Mochtar <karyadi_cm13@apps.ipb.ac.id>
2022-07-04 22:25:07 +0700

    regis3

---
+++ app/src/main/java/id/finalproject/binar/secondhand/fragment/auth/RegisViewModel.kt
@@ -0,0 +1,32 @@
+package id.finalproject.binar.secondhand.fragment.auth
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import id.finalproject.binar.secondhand.model.network.Resource
+import id.finalproject.binar.secondhand.repository.UserRepo
+import kotlinx.coroutines.Dispatchers
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+
+class RegisViewModel(private val userRepo: UserRepo): ViewModel() {
+    fun postRegisUser(
+        full_name: RequestBody,
+        email: RequestBody,
+        password: RequestBody,
+        phone_number: RequestBody,
+        address: RequestBody,
+        image: MultipartBody.Part,
+        city: RequestBody
+    ) = liveData(Dispatchers.IO) {
+        emit(Resource.loading(null))
+        try {
+            emit(Resource.success(userRepo.postRegis(
+                full_name, email, password, phone_number,
+                address, image, city)))
+        } catch (e: Exception) {
+            emit(Resource.error(
+                data = null,
+                message = e.message ?: "Gagal dimuat!"))
+        }
+    }
+}
