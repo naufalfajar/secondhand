9efdb473ebc52e9eca4549a0c05d274539664e85
I'zaz Dhiya'Ulhaq <izazdhiya16@gmail.com>
2022-07-15 18:45:53 +0700

    adapter category

--- app/src/main/java/id/finalproject/binar/secondhand/adapter/HomeCategoryAdapter.kt
+++ app/src/main/java/id/finalproject/binar/secondhand/adapter/HomeCategoryAdapter.kt
@@ -84,57 +84,8 @@ class HomeCategoryAdapter(
                     )
                 }
 
-
-//                if (selected == position) {
-//                    itemCategory.setBackgroundColor(R.color.dark_purple)
-//                    tvCategory.setTextColor(R.color.white)
-//                } else {
-//                    itemCategory.setBackgroundColor(R.color.light_purple)
-//                    tvCategory.setTextColor(R.color.black)
-//                }
-
             }
         }
     }
 
 }
-
-//class HomeCategoryAdapter :
-//    ListAdapter<Category, HomeCategoryAdapter.CategoryViewHolder>(ProductComparator()) {
-//
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
-//        val binding =
-//            ItemCategoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-//        return CategoryViewHolder(binding)
-//    }
-//
-//    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
-//        val currentItem = getItem(position)
-//        if (currentItem != null) {
-//            holder.bind(currentItem)
-//        }
-//    }
-//
-//    class CategoryViewHolder(private val binding: ItemCategoryBinding) :
-//        RecyclerView.ViewHolder(binding.root) {
-//
-//        fun bind(category: Category) {
-//            binding.apply {
-//                btCategory.text = category.name
-//
-////                itemProduct.setOnClickListener {
-////                    onclicklistener.invoke(product.id, product)
-////                }
-//            }
-//        }
-//    }
-//
-//    class ProductComparator : DiffUtil.ItemCallback<Category>() {
-//        override fun areItemsTheSame(oldItem: Category, newItem: Category) =
-//            oldItem.name == newItem.name
-//
-//        override fun areContentsTheSame(oldItem: Category, newItem: Category) =
-//            oldItem == newItem
-//    }
-//
-//}
