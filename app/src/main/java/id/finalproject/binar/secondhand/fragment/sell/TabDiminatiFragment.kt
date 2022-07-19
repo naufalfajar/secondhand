1616c02dd973ae6daa0553385a22a6a6bce1533d
I'zaz Dhiya'Ulhaq <izazdhiya16@gmail.com>
2022-07-19 18:07:10 +0700

    fix ui daftar jual

--- app/src/main/java/id/finalproject/binar/secondhand/fragment/sell/TabDiminatiFragment.kt
+++ app/src/main/java/id/finalproject/binar/secondhand/fragment/sell/TabDiminatiFragment.kt
@@ -1,23 +1,11 @@
 package id.finalproject.binar.secondhand.fragment.sell
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
-import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
-import androidx.navigation.Navigation
-import androidx.recyclerview.widget.LinearLayoutManager
-import binar.lima.satu.secondhand.data.utils.Status
-import binar.lima.satu.secondhand.model.seller.order.GetSellerOrderResponseItem
-import binar.lima.satu.secondhand.view.adapter.SellerOrderAdapter
-import id.finalproject.binar.secondhand.fragment.notification.BidderInfoFragment
-import binar.lima.satu.secondhand.viewmodel.ApiViewModel
-import binar.lima.satu.secondhand.viewmodel.UserViewModel
-import id.finalproject.binar.secondhand.R
+import androidx.fragment.app.Fragment
 import id.finalproject.binar.secondhand.databinding.FragmentTabDiminatiBinding
-import id.finalproject.binar.secondhand.model.network.Status
 import id.finalproject.binar.secondhand.repository.network.SellerOrderRepository
 import id.finalproject.binar.secondhand.repository.viewModelsFactory
 import id.finalproject.binar.secondhand.service.ApiClient
@@ -48,46 +36,46 @@ class TabDiminatiFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentTabDiminatiBinding.inflate(inflater, container, false)
-        getDataDiminati()
+//        getDataDiminati()
         return binding.root
     }
 
-    private fun getDataDiminati() {
-        sellerOrderViewModel.getToken().observe(viewLifecycleOwner) { token ->
-            apiService.getOrderSeller(access_token = token,"").observe(viewLifecycleOwner) {
-                when (it.status) {
-                    Status.SUCCESS -> {
-                        val data = it.data
-
-                        val adapter = SellerOrderAdapter { order ->
-                            val mBundle = bundleOf(BidderInfoFragment.EXTRA_ORDER_ID to order.id)
-                            Navigation.findNavController(requireView()).navigate(
-                                R.id.action_daftarJualFragment2_to_bidderInfoFragment,
-                                mBundle
-                            )
-                        }
-                        val list = mutableListOf<GetOrderSellerResponseItem>()
-
-                        for (order in data!!) {
-                            if (order.status == "pending" && order.product.status == "available" || order.status == "success" && order.product.status == "available") {
-                                list.add(order)
-                            }
-                        }
-
-                        binding.ivListKosong.visibility = if (list.isEmpty()) {
-                            View.VISIBLE
-                        } else {
-                            View.INVISIBLE
-                        }
-
-                        adapter.submitData(list)
-                        binding.apply {
-                            rvDiminati.adapter = adapter
-                            rvDiminati.layoutManager = LinearLayoutManager(requireContext())
-                        }
-                    }
-                }
-            }
-        }
-    }
+//    private fun getDataDiminati() {
+//        sellerOrderViewModel.getToken().observe(viewLifecycleOwner) { token ->
+//            apiService.getOrderSeller(access_token = token,"").observe(viewLifecycleOwner) {
+//                when (it.status) {
+//                    Status.SUCCESS -> {
+//                        val data = it.data
+//
+//                        val adapter = SellerOrderAdapter { order ->
+//                            val mBundle = bundleOf(BidderInfoFragment.EXTRA_ORDER_ID to order.id)
+//                            Navigation.findNavController(requireView()).navigate(
+//                                R.id.action_daftarJualFragment2_to_bidderInfoFragment,
+//                                mBundle
+//                            )
+//                        }
+//                        val list = mutableListOf<GetOrderSellerResponseItem>()
+//
+//                        for (order in data!!) {
+//                            if (order.status == "pending" && order.product.status == "available" || order.status == "success" && order.product.status == "available") {
+//                                list.add(order)
+//                            }
+//                        }
+//
+//                        binding.ivListKosong.visibility = if (list.isEmpty()) {
+//                            View.VISIBLE
+//                        } else {
+//                            View.INVISIBLE
+//                        }
+//
+//                        adapter.submitData(list)
+//                        binding.apply {
+//                            rvDiminati.adapter = adapter
+//                            rvDiminati.layoutManager = LinearLayoutManager(requireContext())
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
 }
--- app/src/main/java/id/finalproject/binar/secondhand/fragment/sell/TabTerjualFragment.kt
+++ app/src/main/java/id/finalproject/binar/secondhand/fragment/sell/TabTerjualFragment.kt
@@ -1,13 +1,10 @@
 package id.finalproject.binar.secondhand.fragment.sell
 
-import android.bluetooth.BluetoothStatusCodes.SUCCESS
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.LinearLayoutManager
-import id.finalproject.binar.secondhand.R
+import androidx.fragment.app.Fragment
 import id.finalproject.binar.secondhand.databinding.FragmentTabTerjualBinding
 
 class TabTerjualFragment : Fragment() {
@@ -15,37 +12,37 @@ class TabTerjualFragment : Fragment() {
     private var _binding: FragmentTabTerjualBinding? = null
     private val binding get() = _binding!!
 
-    private val apiViewModel: ApiViewModel by hiltNavGraphViewModels(R.id.nav_main)
-    private val userViewModel: UserViewModel by hiltNavGraphViewModels(R.id.nav_main)
+//    private val apiViewModel: ApiViewModel by hiltNavGraphViewModels(R.id.nav_main)
+//    private val userViewModel: UserViewModel by hiltNavGraphViewModels(R.id.nav_main)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentTabTerjualBinding.inflate(inflater, container, false)
-        getDataTerjual()
+//        getDataTerjual()
         return binding.root
     }
 
-    private fun getDataTerjual() {
-        userViewModel.getToken().observe(viewLifecycleOwner) { token ->
-            apiViewModel.getSellerOrder(token, "accepted").observe(viewLifecycleOwner){
-                when(it.status){
-                    SUC CESS -> {
-                        val data = it.data!!
-
-                        val adapter = TerjualAdapter(){
-
-                        }
-                        adapter.submitData(data)
-                        binding.apply {
-                            rvTerjual.adapter = adapter
-                            rvTerjual.layoutManager = LinearLayoutManager(requireContext())
-                        }
-                    }
-                }
-            }
-        }
-    }
+//    private fun getDataTerjual() {
+//        userViewModel.getToken().observe(viewLifecycleOwner) { token ->
+//            apiViewModel.getSellerOrder(token, "accepted").observe(viewLifecycleOwner){
+//                when(it.status){
+//                    SUCCESS -> {
+//                        val data = it.data!!
+//
+//                        val adapter = TerjualAdapter(){
+//
+//                        }
+//                        adapter.submitData(data)
+//                        binding.apply {
+//                            rvTerjual.adapter = adapter
+//                            rvTerjual.layoutManager = LinearLayoutManager(requireContext())
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
 
 }
