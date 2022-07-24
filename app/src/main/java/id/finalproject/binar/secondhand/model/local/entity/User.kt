303dad69d79696dac818bef57ac7a627c1b003f6
I'zaz Dhiya'Ulhaq <izazdhiya16@gmail.com>
2022-07-24 20:35:42 +0700

    fix

--- app/src/main/java/id/finalproject/binar/secondhand/model/local/entity/User.kt
+++ app/src/main/java/id/finalproject/binar/secondhand/model/local/entity/User.kt
@@ -1,4 +1,20 @@
-//package id.finalproject.binar.secondhand.model.local.entity
-//
-//class User {
-//}
+package id.finalproject.binar.secondhand.model.local.entity
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
+
+@Entity(tableName = "user")
+data class User(
+    @PrimaryKey val id: Int? = 0,
+    val address: String? = null,
+    val city: String? = null,
+    val createdAt: String? = null,
+    val email: String? = null,
+    val full_name: String? = null,
+    val image_url: String? = null,
+    val password: String? = null,
+    val phone_number: Long? = null,
+    val updatedAt: String? = null
+)
