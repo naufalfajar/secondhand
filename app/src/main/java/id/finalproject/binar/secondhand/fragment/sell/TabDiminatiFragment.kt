4399b579852ee9dc4a4ff2d324e9f9257303866f
auliame <auliaramadhani115@gmail.com>
2022-07-25 13:01:11 +0700

    Initial commit

--- app/src/main/java/id/finalproject/binar/secondhand/fragment/sell/TabDiminatiFragment.kt
+++ app/src/main/java/id/finalproject/binar/secondhand/fragment/sell/TabDiminatiFragment.kt
@@ -1,81 +1,104 @@
 package id.finalproject.binar.secondhand.fragment.sell
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
+import androidx.core.os.bundleOf
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import dagger.hilt.android.AndroidEntryPoint
+import id.finalproject.binar.secondhand.fragment.notification.BidderInfoFragment
+import id.finalproject.binar.secondhand.R
+import id.finalproject.binar.secondhand.adapter.SellerOrderAdapter
 import id.finalproject.binar.secondhand.databinding.FragmentTabDiminatiBinding
+import id.finalproject.binar.secondhand.helper.SharedPreferences
+import id.finalproject.binar.secondhand.model.network.response.seller.GetSellerOrderItem
 import id.finalproject.binar.secondhand.repository.network.SellerOrderRepository
 import id.finalproject.binar.secondhand.repository.viewModelsFactory
 import id.finalproject.binar.secondhand.service.ApiClient
 import id.finalproject.binar.secondhand.service.ApiService
+import id.finalproject.binar.secondhand.model.network.Status
 import id.finalproject.binar.secondhand.viewmodel.SellerOrderViewModel
 
-
+@AndroidEntryPoint
 class TabDiminatiFragment : Fragment() {
 
     private var _binding: FragmentTabDiminatiBinding? = null
     private val binding get() = _binding!!
 
     private val apiService: ApiService by lazy { ApiClient.instance }
+    private lateinit var sharedPref: SharedPreferences
 
     private val sellerOrderRepository: SellerOrderRepository by lazy {
         SellerOrderRepository(
             apiService
         )
     }
+
     private val sellerOrderViewModel: SellerOrderViewModel by viewModelsFactory {
         SellerOrderViewModel(
             sellerOrderRepository
         )
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentTabDiminatiBinding.inflate(inflater, container, false)
-//        getDataDiminati()
+
         return binding.root
+
     }
 
-//    private fun getDataDiminati() {
-//        sellerOrderViewModel.getToken().observe(viewLifecycleOwner) { token ->
-//            apiService.getOrderSeller(access_token = token,"").observe(viewLifecycleOwner) {
-//                when (it.status) {
-//                    Status.SUCCESS -> {
-//                        val data = it.data
-//
-//                        val adapter = SellerOrderAdapter { order ->
-//                            val mBundle = bundleOf(BidderInfoFragment.EXTRA_ORDER_ID to order.id)
-//                            Navigation.findNavController(requireView()).navigate(
-//                                R.id.action_daftarJualFragment2_to_bidderInfoFragment,
-//                                mBundle
-//                            )
-//                        }
-//                        val list = mutableListOf<GetOrderSellerResponseItem>()
-//
-//                        for (order in data!!) {
-//                            if (order.status == "pending" && order.product.status == "available" || order.status == "success" && order.product.status == "available") {
-//                                list.add(order)
-//                            }
-//                        }
-//
-//                        binding.ivListKosong.visibility = if (list.isEmpty()) {
-//                            View.VISIBLE
-//                        } else {
-//                            View.INVISIBLE
-//                        }
-//
-//                        adapter.submitData(list)
-//                        binding.apply {
-//                            rvDiminati.adapter = adapter
-//                            rvDiminati.layoutManager = LinearLayoutManager(requireContext())
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        sharedPref = SharedPreferences(requireContext())
+
+        getDataDiminati()
+
+    }
+
+    private fun getDataDiminati() {
+        val token = sharedPref.getToken()
+        sellerOrderViewModel.getOrderSeller(token!!).observe(viewLifecycleOwner) { it ->
+            when (it.status) {
+                Status.SUCCESS -> {
+                    val data = it.data
+
+                    val adapter = SellerOrderAdapter { order ->
+                        val mBundle = bundleOf(BidderInfoFragment.EXTRA_ORDER_ID to order.id)
+                        Navigation.findNavController(requireView()).navigate(
+                            R.id.action_daftarJualFragment2_to_bidderInfoFragment,
+                            mBundle
+                        )
+                    }
+                    val list = mutableListOf<GetSellerOrderItem>()
+
+                    for (order in data!!) {
+                        if (order.status == "pending" && order.product.status == "available" || order.status == "success" && order.product.status == "available") {
+                            list.add(order)
+                        }
+                    }
+
+                    binding.ivListKosong.visibility = if (list.isEmpty()) {
+                        View.VISIBLE
+                    } else {
+                        View.INVISIBLE
+                    }
+
+                    adapter.submitData(list)
+                    binding.apply {
+                        rvDiminati.adapter = adapter
+                        rvDiminati.layoutManager = LinearLayoutManager(requireContext())
+                    }
+                }
+                else -> {}
+            }
+        }
+    }
 }
--- app/src/main/java/id/finalproject/binar/secondhand/fragment/sell/TabTerjualFragment.kt
+++ app/src/main/java/id/finalproject/binar/secondhand/fragment/sell/TabTerjualFragment.kt
@@ -1,75 +1,87 @@
 package id.finalproject.binar.secondhand.fragment.sell
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.view.isVisible
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
-import id.finalproject.binar.secondhand.adapter.SellListHistoryAdapter
+import id.finalproject.binar.secondhand.model.network.Status
+import id.finalproject.binar.secondhand.adapter.TerjualAdapter
 import id.finalproject.binar.secondhand.databinding.FragmentTabTerjualBinding
-import id.finalproject.binar.secondhand.model.local.entity.History
-import id.finalproject.binar.secondhand.util.Resource
-import id.finalproject.binar.secondhand.viewmodel.SellListViewModel
+import id.finalproject.binar.secondhand.helper.SharedPreferences
+import id.finalproject.binar.secondhand.model.network.response.seller.GetSellerOrderItem
+import id.finalproject.binar.secondhand.repository.network.SellerOrderRepository
+import id.finalproject.binar.secondhand.repository.viewModelsFactory
+import id.finalproject.binar.secondhand.service.ApiClient
+import id.finalproject.binar.secondhand.service.ApiService
+import id.finalproject.binar.secondhand.viewmodel.SellerOrderViewModel
 
 @AndroidEntryPoint
 class TabTerjualFragment : Fragment() {
+
     private var _binding: FragmentTabTerjualBinding? = null
     private val binding get() = _binding!!
 
-    private val sellListViewModel: SellListViewModel by viewModels()
+    private val apiService: ApiService by lazy { ApiClient.instance }
+    private lateinit var sharedPref: SharedPreferences
 
-    private lateinit var sellListHistoryAdapter: SellListHistoryAdapter
+    private val sellerOrderRepository: SellerOrderRepository by lazy {
+        SellerOrderRepository(
+            apiService
+        )
+    }
 
+    private val sellerOrderViewModel: SellerOrderViewModel by viewModelsFactory {
+        SellerOrderViewModel(
+            sellerOrderRepository
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentTabTerjualBinding.inflate(inflater, container, false)
-        return binding.root
-    }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        observeHistory()
+        sharedPref = SharedPreferences(requireContext())
+
+        getDataTerjual()
 
     }
 
-    private fun observeHistory() {
+    private fun getDataTerjual() {
+        val token = sharedPref.getToken()
+        sellerOrderViewModel.getOrderSeller(token!!).observe(viewLifecycleOwner) { it ->
+            when(it.status){
+                Status.SUCCESS -> {
+                    val data = it.data!!
 
-        sellListHistoryAdapter = SellListHistoryAdapter { id: Int, history: History ->
-            val bundle = Bundle()
-            bundle.putInt("id", id)
+                    val adapter = TerjualAdapter(){
 
-        }
+                    }
 
-        binding.apply {
-            listHistory.apply {
-                adapter = sellListHistoryAdapter
-                layoutManager = LinearLayoutManager(requireContext())
-            }
+                    val list = mutableListOf<GetSellerOrderItem>()
+                    for (order in data!!) {
+                        if (order.status == "accepted") {
+                            list.add(order)
+                        }
+                    }
 
-            sellListViewModel.getHistory.observe(viewLifecycleOwner) { result ->
-                sellListHistoryAdapter.updateData(result.data!!)
-                progressBar.isVisible = result is Resource.Loading && result.data.isNullOrEmpty()
-                if (result is Resource.Error && result.data.isNullOrEmpty()) {
-                    Toast.makeText(
-                        requireContext(),
-                        result.error?.localizedMessage,
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    adapter.submitData(data)
+                    binding.apply {
+                        rvTerjual.adapter = adapter
+                        rvTerjual.layoutManager = LinearLayoutManager(requireContext())
+                    }
                 }
+                else -> {}
             }
         }
     }
